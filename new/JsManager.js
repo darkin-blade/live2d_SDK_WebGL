f69fdc95f289aa3fb238b7d146facfccce33e845
Lynx <1197225628@qq.com>
2019-04-17 09:43:12 +0800

    统一用JSManager加载

--- new/JsManager.js
+++ new/JsManager.js
@@ -10,7 +10,7 @@ document.write("<script src=\"" + "new/src/" + "/LAppLive2DManager.js\"></script
 document.write("<script src=\"" + "new/src/" + "/SampleApp.js\"></script>");
 
 var minTips = 2;
-var maxTips = 4;
+var maxTips = 7;
 var thisMy = new Array();
 
 function sampleManager()
@@ -18,6 +18,33 @@ function sampleManager()
     var i = 0;
     for (i = minTips; i <= maxTips; i++)
     {
+        var tempDrag = document.createElement("div");
+        tempDrag.id = "drag_" + i;
+        tempDrag.className = "drag";
+        var tempTip = document.createElement("div");
+        tempTip.id = "tip_" + i;
+        tempTip.className = "tips";
+        tempDrag.appendChild(tempTip);
+        var tempCanvas = document.createElement("canvas");
+        tempCanvas.id = "glcanvas_" + i;
+        tempCanvas.className = "glcanvas";
+        tempCanvas.width = 200;
+        tempCanvas.height = 300;
+        tempDrag.appendChild(tempCanvas);
+        var tempButton = document.createElement("button");
+        tempButton.id = "btnChange_" + i;
+        tempButton.className = "active btnChange";
+        tempDrag.appendChild(tempButton);
+        document.body.appendChild(tempDrag);
         setTimeout("new sampleApp(" + i + ")", (i - 2) * 300);
     }
+    setTimeout("myDrag()", maxTips * 300);
+}
+
+function myDrag()
+{
+    $(".drag").draggable(
+    {
+        containment: document.body
+    });
 }
