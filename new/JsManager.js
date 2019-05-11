992e5fc723ef501f1441dcca142c216530b6d80e
Lynx <1197225628@qq.com>
2019-05-11 09:03:13 +0800

    LDfine

--- new/JsManager.js
+++ new/JsManager.js
@@ -6,194 +6,196 @@ document.write("<link rel=\"stylesheet\" type=\"text/css\" href=\"" + apiAdress 
 document.write("<link rel=\"stylesheet\" type=\"text/css\" href=\"" + apiAdress + "new/tips.css\">");
 document.write("<script async src=\"" + apiAdress + "new/tips/tips.js\"></script>");
 
-document.write("<script src=\"" + apiAdress + "new/src/" + "/live2d.js\"></script>");
-document.write("<script src=\"" + apiAdress + "new/src/" + "/LAppDefine.js\"></script>");
-document.write("<script src=\"" + apiAdress + "new/src/" + "/Live2DFramework.js\"></script>");
-document.write("<script src=\"" + apiAdress + "new/src/" + "/MatrixStack.js\"></script>");
-document.write("<script src=\"" + apiAdress + "new/src/" + "/ModelSettingJson.js\"></script>");
-document.write("<script src=\"" + apiAdress + "new/src/" + "/PlatformManager.js\"></script>");
-document.write("<script src=\"" + apiAdress + "new/src/" + "/LAppModel.js\"></script>");
-document.write("<script src=\"" + apiAdress + "new/src/" + "/LAppLive2DManager.js\"></script>");
-document.write("<script src=\"" + apiAdress + "new/src/" + "/SampleApp.js\"></script>");
+// document.write("<script src=\"" + apiAdress + "new/src/" + "/live2d.js\"></script>");
+// document.write("<script src=\"" + apiAdress + "new/src/" + "/LAppDefine.js\"></script>");
+// document.write("<script src=\"" + apiAdress + "new/src/" + "/Live2DFramework.js\"></script>");
+// document.write("<script src=\"" + apiAdress + "new/src/" + "/MatrixStack.js\"></script>");
+// document.write("<script src=\"" + apiAdress + "new/src/" + "/ModelSettingJson.js\"></script>");
+// document.write("<script src=\"" + apiAdress + "new/src/" + "/PlatformManager.js\"></script>");
+// document.write("<script src=\"" + apiAdress + "new/src/" + "/LAppModel.js\"></script>");
+// document.write("<script src=\"" + apiAdress + "new/src/" + "/LAppLive2DManager.js\"></script>");
+// document.write("<script src=\"" + apiAdress + "new/src/" + "/SampleApp.js\"></script>");
 
-// document.write("<script src=\"" + apiAdress + "new/" + "/main.js\"></script>");
+document.write("<script src=\"" + apiAdress + "new/" + "/main.js\"></script>");
 
 var minNum = 0;
 var maxNum = 2;
 var thisMy = new Array();
 var JsMgr = {
-    loadInterval: 300,
-    myRequest: "",
-    border: 3,
+  loadInterval: 300,
+  myRequest: "",
+  border: 3,
 }
 
 function sampleManager()
 {
-    // fileLoad();
-    divCreate();
+  // fileLoad();
+  divCreate();
 }
 
 function divCreate()
 {
-    var i = 0;
-    for (i = minNum; i <= maxNum; i++)
-    {
-        // 拖拽元素
-        var tempDrag = document.createElement("div");
-        tempDrag.id = "drag_" + i;
-        tempDrag.className = "drag";
-        
-        // 拖拽元素的css
-        var tempCss = document.createElement("style");
-        tempCss.innerHTML = 
-        "#drag_" + i + " { width: " + LAppDefine[i].width + "px; " + "height: " + LAppDefine[i].height + "px; }";
-        document.body.appendChild(tempCss);
+  var i = 0;
+  for (i = minNum; i <= maxNum; i++)
+  {
+    // 拖拽元素
+    var tempDrag = document.createElement("div");
+    tempDrag.id = "drag_" + i;
+    tempDrag.className = "drag";
 
-        // tip元素
-        var tempTip = document.createElement("div");
-        tempTip.mystop = 0;
-        tempTip.id = "tip_" + i;
-        tempTip.className = "tips";
-        tempTip.setAttribute("style",
-        "width: " + (LAppDefine[i].width + 50) + "px;" + 
-        "left: " + (-25) + "px;" + 
-        "margin: 0px 0px " + (-LAppDefine[i].height / (10 * (LAppDefine[i].width / LAppDefine[i].height))) + "px 0px;"
-        );// 自动计算大小
-        tempDrag.appendChild(tempTip);
-        
-        // live2d画布
-        var tempCanvas = document.createElement("canvas");
-        tempCanvas.id = "glcanvas_" + i;
-        tempCanvas.className = "glcanvas";
-        tempCanvas.width = LAppDefine[i].width;
-        tempCanvas.height = LAppDefine[i].height;
-        tempDrag.appendChild(tempCanvas);
-        
-        // 对齐用,临时变量
-        var tempButton = document.createElement("div");
-        
-        // 切换按钮
-        var tempChange = document.createElement("div");
-        tempChange.id = "btnChange_" + i;
-        tempChange.className = "btnChange myBtn";
-        tempChange.setAttribute("style", "width: " + (LAppDefine[i].width / 3 - 2 * JsMgr.border) + "px;" + 
-        "left: " + 0 + "px;");
-        tempButton.appendChild(tempChange);
-        
-        // 删除按钮
-        var tempClose = document.createElement("div");
-        tempClose.id = "btnClose_" + i;
-        tempClose.className = "btnClose myBtn";
-        tempClose.setAttribute("style", "width: " + (LAppDefine[i].width / 3 - 2 * JsMgr.border) + "px;" + 
-        "left: " + (LAppDefine[i].width / 3) + "px;");
-        tempClose.setAttribute("onclick", "deleteFather(" + i + ")");
-        tempClose.textContent = "close";
-        tempButton.appendChild(tempClose);
+    // 拖拽元素的css
+    var tempCss = document.createElement("style");
+    tempCss.innerHTML =
+      "#drag_" + i + " { width: " + LAppDefine[i].width + "px; " + "height: " + LAppDefine[i].height + "px; }";
+    document.body.appendChild(tempCss);
 
-        // 切换canvas大小按钮
-        var tempHide = document.createElement("div");
-        tempHide.id = "btnHide_" + i;
-        tempHide.className = "btnHide myBtn";
-        tempHide.setAttribute("style", "width: " + (LAppDefine[i].width / 3 - 2 * JsMgr.border) + "px;" + 
-        "left: " + (LAppDefine[i].width * 2 / 3) + "px;");
-        tempHide.setAttribute("onclick", "myHide(" + i + ")");
-        tempHide.textContent = "hide";
-        tempButton.appendChild(tempHide);
-        
-        // 添加至body
-        tempDrag.appendChild(tempButton);
-        document.body.appendChild(tempDrag);
-        document.getElementById("drag_" + i).width = LAppDefine[i].width;
+    // tip元素
+    var tempTip = document.createElement("div");
+    tempTip.mystop = 0;
+    tempTip.id = "tip_" + i;
+    tempTip.className = "tips";
+    tempTip.setAttribute("style",
+      "width: " + (LAppDefine[i].width + 50) + "px;" +
+      "left: " + (-25) + "px;" +
+      "margin: 0px 0px " + (-LAppDefine[i].height / (10 * (LAppDefine[i].width / LAppDefine[i].height))) + "px 0px;"
+    ); // 自动计算大小
+    tempDrag.appendChild(tempTip);
 
-        // 主体函数
-        thisMy[i] = new sampleApp(i);
-        setTimeout("thisMy[" + i + "].mystart()", (i - minNum) * JsMgr.loadInterval);
-    }
-    setTimeout("myDrag()", (maxNum - minNum + 1) * JsMgr.loadInterval);
+    // live2d画布
+    var tempCanvas = document.createElement("canvas");
+    tempCanvas.id = "glcanvas_" + i;
+    tempCanvas.className = "glcanvas";
+    tempCanvas.width = LAppDefine[i].width;
+    tempCanvas.height = LAppDefine[i].height;
+    tempDrag.appendChild(tempCanvas);
+
+    // 对齐用,临时变量
+    var tempButton = document.createElement("div");
+
+    // 切换按钮
+    var tempChange = document.createElement("div");
+    tempChange.id = "btnChange_" + i;
+    tempChange.className = "btnChange myBtn";
+    tempChange.setAttribute("style", "width: " + (LAppDefine[i].width / 3 - 2 * JsMgr.border) + "px;" +
+      "left: " + 0 + "px;");
+    tempButton.appendChild(tempChange);
+
+    // 删除按钮
+    var tempClose = document.createElement("div");
+    tempClose.id = "btnClose_" + i;
+    tempClose.className = "btnClose myBtn";
+    tempClose.setAttribute("style", "width: " + (LAppDefine[i].width / 3 - 2 * JsMgr.border) + "px;" +
+      "left: " + (LAppDefine[i].width / 3) + "px;");
+    tempClose.setAttribute("onclick", "deleteFather(" + i + ")");
+    tempClose.textContent = "close";
+    tempButton.appendChild(tempClose);
+
+    // 切换canvas大小按钮
+    var tempHide = document.createElement("div");
+    tempHide.id = "btnHide_" + i;
+    tempHide.className = "btnHide myBtn";
+    tempHide.setAttribute("style", "width: " + (LAppDefine[i].width / 3 - 2 * JsMgr.border) + "px;" +
+      "left: " + (LAppDefine[i].width * 2 / 3) + "px;");
+    tempHide.setAttribute("onclick", "myHide(" + i + ")");
+    tempHide.textContent = "hide";
+    tempButton.appendChild(tempHide);
+
+    // 添加至body
+    tempDrag.appendChild(tempButton);
+    document.body.appendChild(tempDrag);
+    document.getElementById("drag_" + i).width = LAppDefine[i].width;
+
+    // 主体函数
+    thisMy[i] = new sampleApp(i);
+    setTimeout("thisMy[" + i + "].mystart()", (i - minNum) * JsMgr.loadInterval);
+  }
+  setTimeout("myDrag()", (maxNum - minNum + 1) * JsMgr.loadInterval);
 }
 
 function myDrag()
 {
-    $(".drag").draggable(
-    {
-        containment: document.body
-    });
+  $(".drag").draggable(
+  {
+    containment: document.body
+  });
 }
 
 function deleteFather(num)
 {
-    var tempNode = document.getElementById("drag_" + num);
-    // tempNode.parentNode.removeChild(tempNode);
-    tempNode.setAttribute("style", "display: none;");
+  var tempNode = document.getElementById("drag_" + num);
+  // tempNode.parentNode.removeChild(tempNode);
+  tempNode.setAttribute("style", "display: none;");
 }
 
 function myHide(num)
 {
-    var tempTip = document.getElementById("tip_" + num);
-    tempTip.mystop = 1;
-    tempTip.style.opacity = 0;
-    var tempBtn = document.getElementById("btnHide_" + num);
-    tempBtn.className = "btnShow myBtn";
-    tempBtn.textContent = "show";
-    tempBtn.setAttribute("onclick", "myShow(" + num + ")");
+  var tempTip = document.getElementById("tip_" + num);
+  tempTip.mystop = 1;
+  tempTip.style.opacity = 0;
+  var tempBtn = document.getElementById("btnHide_" + num);
+  tempBtn.className = "btnShow myBtn";
+  tempBtn.textContent = "show";
+  tempBtn.setAttribute("onclick", "myShow(" + num + ")");
 }
 
 function myShow(num)
 {
-    var tempTip = document.getElementById("tip_" + num);
-    tempTip.mystop = 0;
-    var tempBtn = document.getElementById("btnHide_" + num);
-    tempBtn.className = "btnHide myBtn";
-    tempBtn.textContent = "hide";
-    tempBtn.setAttribute("onclick", "myHide(" + num + ")");
+  var tempTip = document.getElementById("tip_" + num);
+  tempTip.mystop = 0;
+  var tempBtn = document.getElementById("btnHide_" + num);
+  tempBtn.className = "btnHide myBtn";
+  tempBtn.textContent = "hide";
+  tempBtn.setAttribute("onclick", "myHide(" + num + ")");
 }
 
 function fileLoad()
 {
-    $("<link>")
-        .attr(
-        {
-            rel: "stylesheet",
-            type: "text/css",
-            href: apiAdress + "new/live2d.css"
-        })
-        .appendTo("head");
-    $("<link>")
-        .attr(
-        {
-            rel: "stylesheet",
-            type: "text/css",
-            href: apiAdress + "new/tips.css"
-        })
-        .appendTo("head");
-    var jsList = [
-        // "new/main.js",
-        "/live2d.js",
-        "/LAppDefine.js",
-        "/Live2DFramework.js",
-        "/MatrixStack.js",
-        "/ModelSettingJson.js",
-        "/PlatformManager.js",
-        "/LAppModel.js",
-        "/LAppLive2DManager.js",
-        "/SampleApp.js"];
-    for (var i = 0; i < jsList.length; i ++)
+  $("<link>")
+    .attr(
     {
-        $.ajax({
-            url: apiAdress + "new/src" + jsList[i],
-            dataType: "text",
-            async: false,
-            success: function(result)
-            {
-                JsMgr.myRequest += result;
-                if (i + 1 == jsList.length)
-                {
-                    var tempScript = document.createElement("script");
-                    tempScript.innerHTML = JsMgr.myRequest;
-                    document.body.appendChild(tempScript);
-                }
-            }
-        });
-    } 
-    $.getScript("new/tips/tips.js");
+      rel: "stylesheet",
+      type: "text/css",
+      href: apiAdress + "new/live2d.css"
+    })
+    .appendTo("head");
+  $("<link>")
+    .attr(
+    {
+      rel: "stylesheet",
+      type: "text/css",
+      href: apiAdress + "new/tips.css"
+    })
+    .appendTo("head");
+  var jsList = [
+    // "new/main.js",
+    "/live2d.js",
+    "/LAppDefine.js",
+    "/Live2DFramework.js",
+    "/MatrixStack.js",
+    "/ModelSettingJson.js",
+    "/PlatformManager.js",
+    "/LAppModel.js",
+    "/LAppLive2DManager.js",
+    "/SampleApp.js"
+  ];
+  for (var i = 0; i < jsList.length; i++)
+  {
+    $.ajax(
+    {
+      url: apiAdress + "new/src" + jsList[i],
+      dataType: "text",
+      async: false,
+      success: function(result)
+      {
+        JsMgr.myRequest += result;
+        if (i + 1 == jsList.length)
+        {
+          var tempScript = document.createElement("script");
+          tempScript.innerHTML = JsMgr.myRequest;
+          document.body.appendChild(tempScript);
+        }
+      }
+    });
+  }
+  $.getScript("new/tips/tips.js");
 }
