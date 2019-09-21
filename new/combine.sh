36642d4cef9f66bbe6c6a9c02593868859637ee8
aaaatrox <2104934038@qq.com>
2019-09-21 18:42:08 +0800

    更改main的输出位置

--- new/combine.sh
+++ new/combine.sh
@@ -1,27 +1,27 @@
-cat extension/JsManager.js > main.js
-echo "\n" >> main.js
-cat src/LAppDefine.js >> main.js
-echo "\n" >> main.js
-cat src/live2d.js >> main.js
-echo "\n" >> main.js
-cat src/LAppLive2DManager.js >> main.js
-echo "\n" >> main.js
-cat src/Live2DFramework.js >> main.js
-echo "\n" >> main.js
-cat src/LAppModel.js >> main.js
-echo "\n" >> main.js
-cat src/MatrixStack.js >> main.js
-echo "\n" >> main.js
-cat src/ModelSettingJson.js >> main.js
-echo "\n" >> main.js
-cat src/PlatformManager.js >> main.js
-echo "\n" >> main.js
-cat src/SampleApp.js >> main.js
-echo "\n" >> main.js
-# cat extension/tips.js >> main.js
-# echo "\n" >> main.js
+cat extension/JsManager.js > ../output/main.js
+echo "\n" >> ../output/main.js
+cat src/LAppDefine.js >> ../output/main.js
+echo "\n" >> ../output/main.js
+cat src/live2d.js >> ../output/main.js
+echo "\n" >> ../output/main.js
+cat src/LAppLive2DManager.js >> ../output/main.js
+echo "\n" >> ../output/main.js
+cat src/Live2DFramework.js >> ../output/main.js
+echo "\n" >> ../output/main.js
+cat src/LAppModel.js >> ../output/main.js
+echo "\n" >> ../output/main.js
+cat src/MatrixStack.js >> ../output/main.js
+echo "\n" >> ../output/main.js
+cat src/ModelSettingJson.js >> ../output/main.js
+echo "\n" >> ../output/main.js
+cat src/PlatformManager.js >> ../output/main.js
+echo "\n" >> ../output/main.js
+cat src/SampleApp.js >> ../output/main.js
+echo "\n" >> ../output/main.js
+# cat extension/tips.js >> ../output/main.js
+# echo "\n" >> ../output/main.js
 
-cat extension/live2d.css > main.css
-echo "\n" >> main.css
-# cat extension/tips.css >> main.css
-# echo "\n" >> main.css
+cat extension/live2d.css > ../output/main.css
+echo "\n" >> ../output/main.css
+# cat extension/tips.css >> ../output/main.css
+# echo "\n" >> ../output/main.css
